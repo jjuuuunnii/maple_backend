a2a89e3c5dbc7769aa7d0540b7c8a5904fbb1e79
jjuuuunnii <jake991110@naver.com>
2023-09-15 11:16:35 +0900

    에러 수정

--- src/main/java/com/maple/exception/jwt/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/maple/exception/jwt/CustomAuthenticationEntryPoint.java
@@ -6,10 +6,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.json.JSONObject;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
 @Slf4j
+@Component
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
     @Override
