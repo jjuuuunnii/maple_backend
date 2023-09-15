d445d4ecfcb2da55211068f56ce878138702c197
jjuuuunnii <jake991110@naver.com>
2023-09-15 13:57:06 +0900

    오류 수정

--- src/main/java/com/maple/exception/jwt/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/maple/exception/jwt/CustomAuthenticationEntryPoint.java
@@ -16,7 +16,7 @@ public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint 
 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
-        log.info("request.getRequestURI() = {} ", request.getRequestURI());
+        log.error("request.getRequestURI() = {} ", request.getRequestURI());
 
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
         response.setCharacterEncoding("UTF-8");
